0c3137d5d6e72117323d0ce464285cd4be2629a9
user-2020-122 <2941959130@qq.com>
2021-05-03 20:22:02 +0800

    组件渲染、

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,50 @@
 <template>
   <div id="container">
-
+    <column-list :list="list"/>
   </div>
-<!--  <router-view/>-->
 </template>
 
 <script lang="ts">
- import {defineComponent} from 'vue'
- import  "bootstrap/dist/css/bootstrap.min.css"
- import ColumnList from "./components/ColumnList.vue";
- import { ColumProps } from "@/components/ColumnList.vue";
-  const testData: ColumProps[] = [
-    {
-      id:1,
-      title:'这是仿知乎的专栏',
-      description:"这是仿知乎的专栏,有一段非常有意思的介绍,可以更新一下嗷",
-      avatar:"",
-    },
-    {
-     id:1,
-     title:'这是仿知乎的专栏',
-     description:"这是仿知乎的专栏,有一段非常有意思的介绍,可以更新一下嗷",
-     avatar:"",
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumProps } from './components/ColumnList.vue'
+const testData: ColumProps[] = [
+  {
+    id: 1,
+    title: '这是仿知乎的专栏',
+    description: '这是仿知乎的专栏,有一段非常有意思的介绍,可以更新一下嗷',
+    avatar: 'https://vuejs.org/images/logo.png'
+  },
+  {
+    id: 2,
+    title: '这是仿知乎的专栏',
+    description: '这是仿知乎的专栏,有一段非常有意思的介绍,可以更新一下嗷',
+    avatar: 'https://vuejs.org/images/logo.png'
+  },
+  {
+    id: 3,
+    title: '这是仿知乎的专栏',
+    description: '这是仿知乎的专栏,有一段非常有意思的介绍,可以更新一下嗷',
+    avatar: 'https://vuejs.org/images/logo.png'
+  },
+  {
+    id: 4,
+    title: '这是仿知乎的专栏',
+    description: '这是仿知乎的专栏,有一段非常有意思的介绍,可以更新一下嗷',
+    avatar: 'https://vuejs.org/images/logo.png'
+  }
+]
+export default defineComponent({
+  name: 'App',
+  components: {
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData
     }
-  ]
- export  default defineComponent({
-      nameL:"App",
-      components:{
-        ColumnList
-      },
-      setup(){
-          return{
-           list:testData
-          }
-      }
- })
+  }
+})
 </script>
 <style>
 
--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,41 +1,42 @@
 <template>
- <div>
-   <ul>
-     <Li v-for="column in list" :key="column.id">
-       <img :src="column.avatar" :alt="column.title">
-       <h5>{{column.title}}</h5>
-        <p>{{column.description}}</p>
-       <a href="#">进入专栏</a>
-     </Li>
-   </ul>
- </div>
+   <div class="row">
+       <div v-for="column in list" :key="column.id" class="col-4">
+         <div class="card">
+         <img :src="column.avatar" :alt="column.title" class="rounded-circle border border-light w-25 my-3">
+         <h5 class="card-title">{{column.title}}</h5>
+         <p class="card-text">{{column.description}}</p>
+         <a href="#" class="btn btn-primary">进入专栏</a>
+       </div>
+     </div>
+   </div>
 </template>
 
 <script lang="ts">
-  //PropType专门搞成泛型的
-  import  {defineComponent,PropType} from 'vue'
-  export  interface ColumProps{
-     id:number;
-     title:string;
-     avatar:string;
-     description:string;
+// PropType专门搞成泛型的
+import { defineComponent, PropType } from 'vue'
+export interface ColumProps{
+     id:number,
+     title:string,
+     avatar:string,
+     description:string
   }
-  //组件props 需要自己定义的接口和类型的属性验证
-  export default defineComponent({
-     name: "ColumnList",
-     props:{
-      list:{
-        type:Array as PropType<ColumProps[]>,
-        required:true
-      }
-     },
-    setup(props){
-
+  // 组件props 需要自己定义的接口和类型的属性验证
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<ColumProps[]>,
+      required: true
     }
-    }
-  )
+  }
+  // setup (props) {
+  // }
+})
 </script>
 
 <style scoped>
-
+img{
+  width: 73px;
+  height: 70px;
+}
 </style>
